fe6df5ffef32000d90b729877cdb059f7759d565
ramkaren <114192954+ramkaren@users.noreply.github.com>
2025-02-13 01:25:32 -0600

    Add files via upload

---
+++ programa1.py
@@ -0,0 +1,5 @@
+#Suma de dos valores
+num1 = float(input("Ingresa el primer valor:  "))
+num2 = float(input("Ingresa el segundo valor:  "))
+suma = num1 + num2
+print("La suma de los dos valores es:  ", suma)
